625f12d4d4fef1871d10cf2f94b0f6b4ee6fb872
ashishsharma157 <sharma.ashish78@gmail.com>
2024-09-14 11:34:39 -0700

    SSG static site generation

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,36 @@
 import MeetupList from "../components/meetups/MeetupList";
 import Layout from '../components/layout/Layout';
 
-function HomePage() {
-  const DUMMY = [
-    {
-      id: "m1",
-      title: "A first meet up",
-      image:
-        "https://cdn.prod.website-files.com/619c916dd7a3fa284adc0b27/65fadedbb6aa925d4ffaf3f0_fb350b1d-34b7-466b-a693-dbf3af0d93f7.jpeg",
-      address: "Some address 5, some city",
-      description: "This is first meet up",
-    },
-    {
-      id: "m2",
-      title: "A second meet up",
-      image:
-        "https://cdn.prod.website-files.com/619c916dd7a3fa284adc0b27/65fadedbb6aa925d4ffaf3f0_fb350b1d-34b7-466b-a693-dbf3af0d93f7.jpeg",
-      address: "Some address 10, some city 1",
-      description: "This is second meet up",
-    },
-  ];
-  return <MeetupList meetups={DUMMY} />;
+const DUMMY = [
+  {
+    id: "m1",
+    title: "A first meet up",
+    image:
+      "https://cdn.prod.website-files.com/619c916dd7a3fa284adc0b27/65fadedbb6aa925d4ffaf3f0_fb350b1d-34b7-466b-a693-dbf3af0d93f7.jpeg",
+    address: "Some address 5, some city",
+    description: "This is first meet up",
+  },
+  {
+    id: "m2",
+    title: "A second meet up",
+    image:
+      "https://cdn.prod.website-files.com/619c916dd7a3fa284adc0b27/65fadedbb6aa925d4ffaf3f0_fb350b1d-34b7-466b-a693-dbf3af0d93f7.jpeg",
+    address: "Some address 10, some city 1",
+    description: "This is second meet up",
+  },
+];
+function HomePage(props) {
+
+  return <MeetupList meetups={props.meetups} />;
 }
 
+export async function getStaticProps(){
+  return{
+    props:{
+      meetups: DUMMY
+    },
+    revalidate:10
+  }
+
+}
 export default HomePage;
