2a572ad421f0df73bf469b1cc8e6c0c19f1e37d9
ashishsharma157 <sharma.ashish78@gmail.com>
2024-09-14 13:31:51 -0700

    get single record from mongodb

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -1,3 +1,5 @@
+import { MongoClient, ObjectId } from "mongodb";
+
 import MeetupDetail from "../components/meetups/MeetupDetail";
 
 function MeetupDetails(props) {
@@ -12,34 +14,39 @@ function MeetupDetails(props) {
 }
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://sharmaashish78:9Ha0CVJBwxBYfQj9@cluster0.9aior.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const data = await meetupsCollection.find({}, { _id: 1 }).toArray();
+  client.close();
   return {
-    fallback:false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    fallback: false,
+    paths: data.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
 
 export async function getStaticProps(context) {
   const id = context.params.meetupId;
+  const client = await MongoClient.connect(
+    "mongodb+srv://sharmaashish78:9Ha0CVJBwxBYfQj9@cluster0.9aior.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+  const data = await meetupsCollection.findOne({ _id: new ObjectId(id), });
+  client.close();
+
   return {
     props: {
       meetupData: {
-        image:
-          "https://cdn.prod.website-files.com/619c916dd7a3fa284adc0b27/65fadedbb6aa925d4ffaf3f0_fb350b1d-34b7-466b-a693-dbf3af0d93f7.jpeg",
-        title: "The first meet up",
-        id:id,
-        address: "Some address 5, Some City",
-        description: "Meetup description",
+        id: data._id.toString(),
+        title: data.title,
+        description: data.description,
+        address: data.address,
+        image: data.image,
       },
     },
   };
